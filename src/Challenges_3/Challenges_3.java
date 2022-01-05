7476c4ff5f4c7b9ce089fce8bb008a1924769fdc
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-04 23:11:10 -0300

    Challenges_3

---
+++ src/Challenges_3/Challenges_3.java
@@ -0,0 +1,10 @@
+package Challenges_3;
+
+public class Challenges_3 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
