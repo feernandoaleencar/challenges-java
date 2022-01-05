f1e2872dea82fed941411878a3ac9e8c025b2fd4
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-05 00:26:33 -0300

    Challenges_4

--- src/Challenges_4/challenges_4.java
+++ src/Challenges_4/challenges_4.java
@@ -11,13 +11,24 @@ public class challenges_4 {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         scanner.close();
         if (N >= 1 && N <= 100) {
-            if (N % 2 == 0) {
-                System.out.println("Not Weird");
-            } else {
+            if (N == 1) {
                 System.out.println("Weird");
+            } else if (N >= 2 && N <= 5) {
+                if (N % 2 == 0) {
+                    System.out.println("Not Weird");
+                } else {
+                    System.out.println("Weird");
+                }
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else {
+                if (N % 2 == 0) {
+                    System.out.println("Not Weird");
+                } else {
+                    System.out.println("Weird");
+                }
             }
         }
-
     }
 
 }
