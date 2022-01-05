68170d2aa9f47f07f685543e266999d8303db808
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-04 23:49:56 -0300

    Challenges_4

---
+++ src/Challenges_4/challenges_4.java
@@ -0,0 +1,23 @@
+package Challenges_4;
+
+import java.util.Scanner;
+
+public class challenges_4 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+        if (N >= 1 && N <= 100) {
+            if (N % 2 == 0) {
+                System.out.println("Not Weird");
+            } else {
+                System.out.println("Weird");
+            }
+        }
+
+    }
+
+}
