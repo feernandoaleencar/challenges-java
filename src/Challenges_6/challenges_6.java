ed93c1f7c0366266d7b20d9a164f27e71df536bc
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-26 23:37:37 -0300

    Challenges_6

--- src/Challenges_6/challenges_6.java
+++ src/Challenges_6/challenges_6.java
@@ -1,15 +1,18 @@
 package Challenges_6;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class challenges_6 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-
-        bufferedReader.close();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=10; i++)
+        {
+            int result=n*i;
+            System.out.println(n+" x "+i+" = "+result);
+        }
     }
 
 }
