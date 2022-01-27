9f6565cfc247f9cf0d7e9e8a836c7949c0b35876
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-26 23:29:29 -0300

    Challenges_5

--- src/Challenges_5/challenges_5.java
+++ src/Challenges_5/challenges_5.java
@@ -11,7 +11,7 @@ public class challenges_5{
             {
                 String s1=sc.next();
                 int x=sc.nextInt();
-                //Complete this line
+                System.out.printf("%-15s%03d%n", s1, x);
             }
             System.out.println("================================");
 
